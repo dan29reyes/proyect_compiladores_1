2c591f1a86d2b14582914698ac6311d328b68991
dan29reyes <dan29reyes@unitec.edu>
2025-12-07 11:12:28 -0600

    Implementacion de statements assignment, if, while, print

--- AST/AST.cpp
+++ AST/AST.cpp
@@ -15,30 +15,42 @@ namespace AST
 
     std::string Assignment::toString() const
     {
+        return name + " = " + value->toString();
     }
 
     std::string IfStmt::toString() const
     {
+        std::string s = "if (" + cond->toString() + ")" + whenTrueBranch->toString();
+        if (whenFalseBranch)
+        {
+            s += " else " + whenFalseBranch->toString();
+        }
+        return s;
     }
 
     std::string WhileStmt::toString() const
     {
+        return "while (" + cond->toString() + ")" + stmt->toString();
     }
 
     std::string PrintStmt::toString() const
     {
+        return "print(" + expr->toString() + ")";
     }
 
     std::string NumberLiteral::toString() const
     {
+        return std::to_string(value);
     }
 
     std::string Identifier::toString() const
     {
+        return name;
     }
 
     // Expr
     std::string RelationalExpr::toString() const
     {
+        
     }
 };
